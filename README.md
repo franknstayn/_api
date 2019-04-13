438ce9088f2cd6f364992ec6e10fb427ba7f8a91
Frank John Begornia <frank.begornia@yahoo.com>
2019-04-13 16:05:28 +0800

    updated

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ Run this command in the application directory to run the test suite
 	php composer.phar test
 
 That's it! Now go build something cool.
+# _api
--- src/routes.php
+++ src/routes.php
@@ -15,8 +15,7 @@ use Slim\Http\Response;
 
 // get attendance for web display
 $app->get('/attendance', function ($request, $response, $args) {
-    $sth = $this->db->prepare("SELECT a.*, u.firstname, u.lastname, u.profile_picture  FROM attendance AS a 
-    INNER JOIN users AS u ON u.rfid = a.rfid
+    $sth = $this->db->prepare("SELECT a.rfid, DATE_FORMAT(a.datetime_recorded, '%M %e, %Y %r') AS datetime_recorded, a.status, a.date_created, u.firstname, u.lastname, u.profile_picture  FROM attendance AS a INNER JOIN users AS u ON u.rfid = a.rfid
     ORDER BY a.date_created DESC LIMIT 4");
     $sth->execute();
     $attendance = $sth->fetchAll();
@@ -28,12 +27,34 @@ $app->get('/attendance', function ($request, $response, $args) {
 
 
  // insert time_in
- $app->post('/time/in', function ($request, $response) {
+ $app->post('/attendance/add', function ($request, $response) {
     $input = $request->getParsedBody();
-    $sql = "INSERT INTO attendance (rfid, time_id) VALUES (:rfid, :time_in)";
+    // var_dump($input);
+    $sql = "INSERT INTO attendance (rfid, datetime_recorded, status) VALUES (:rfid, :dt_record, :stat)";
     $sth = $this->db->prepare($sql);
-    $sth->bindParam("rfid", $input['rfid'], "time_in", $input['time_in']);
+    $sth->bindParam("rfid", $input['rfid']);
+    $sth->bindParam("dt_record", $input['date_time']);
+    $sth->bindParam("stat", $input['stat']);
     $sth->execute();
     $input['id'] = $this->db->lastInsertId();
-    return $this->response->withJson($input);
+    return $this->response->withJson($input)
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+$app->get('/attendance/[{id}]', function ($request, $response, $args) {
+
+    $sth = $this->db->prepare("SELECT a.rfid, DATE_FORMAT(a.datetime_recorded, '%M %e, %Y %r') AS datetime_recorded, a.status, a.date_created, u.firstname, u.lastname, u.profile_picture  FROM attendance AS a 
+    INNER JOIN users AS u ON u.rfid = a.rfid WHERE a.id = :id
+    ORDER BY a.date_created DESC LIMIT 4");
+    $sth->bindParam("id", $args['id']);
+    $sth->execute();
+    $attendance = $sth->fetchAll();
+    return $this->response->withJson($attendance)
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
 });
